01914582dfb31f736398be2e1646a20c64970590
albgei <77663520+albgei@users.noreply.github.com>
2022-07-02 13:04:24 +0200

    final commit

--- Assets/liveWater.cs
+++ Assets/liveWater.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RectChange : MonoBehaviour
+public class liveWater : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
